b9708b05d0fda440b011de48a799c0099f119b0e
Siffrein Sigy <hello@siffreinsigy.me>
2024-04-10 11:01:57 +0200

    Updated tautulli notifier script

--- scripts/tautulli_notify/tautulli_notify.py
+++ scripts/tautulli_notify/tautulli_notify.py
@@ -12,7 +12,7 @@ Arguments:
     * --notifier_id [int] - optional, Tautulli notifier ID for the script notification agent (default: env var TAUTULLI_NOTIFIER_ID)
     * --tautulli_url [str] - optional, Tautulli URL, e.g. http://localhost:8181 (default: env var TAUTULLI_URL)
     * --tautulli_api_key [str] - optional, Tautulli API key (default: env var TAUTULLI_API_KEY)
-    * --subject [str] - optional, notification subject (default: <b>Tautulli (Black Pearl)</b>)
+    * --subject [str] - optional, notification subject (default: Tautulli Notification)
     * --body [str] - required, notification body
 
 Usage:
@@ -28,20 +28,20 @@ TAUTULLI_URL = os.getenv('TAUTULLI_URL')
 TAUTULLI_API_KEY = os.getenv('TAUTULLI_API_KEY')
 TAUTULLI_NOTIFIER_ID = os.getenv('TAUTULLI_NOTIFIER_ID')
 
-def tautulli_notify(subject, body, apikey=TAUTULLI_API_KEY, notifier_id=TAUTULLI_NOTIFIER_ID, tautulli_url=TAUTULLI_URL):
+def notify(subject, body, api_key=TAUTULLI_API_KEY, notifier_id=TAUTULLI_NOTIFIER_ID, base_url=TAUTULLI_URL):
     # Throw an error if the variables are not set
-    if not tautulli_url or not apikey or not notifier_id:
+    if not base_url or not api_key or not notifier_id:
         raise Exception('Tautulli URL, API key, and notifier ID are required')
 
     params = {
-        "apikey": apikey,
+        "apikey": api_key,
         "cmd": "notify",
         "notifier_id": notifier_id,
         "subject": subject,
         "body": body
     }
 
-    r = requests.get(tautulli_url.rstrip('/') + '/api/v2', params=params)
+    r = requests.get(base_url.rstrip('/') + '/api/v2', params=params)
     r.raise_for_status()
 
     return True
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     parser.add_argument('--notifier_id', type=int, required=False, default=TAUTULLI_NOTIFIER_ID)
     parser.add_argument('--tautulli_url', type=str, required=False, default=TAUTULLI_URL)
     parser.add_argument('--tautulli_api_key', type=str, required=False, default=TAUTULLI_API_KEY)
-    parser.add_argument('--subject', type=str, required=False, default='<b>Tautulli (Black Pearl)</b>')
+    parser.add_argument('--subject', type=str, required=False, default='Tautulli Notification')
     parser.add_argument('--body', type=str, required=True)
     opts = parser.parse_args()
 
@@ -60,4 +60,4 @@ if __name__ == '__main__':
     TAUTULLI_URL = opts.tautulli_url
     TAUTULLI_API_KEY = opts.tautulli_api_key
 
-    tautulli_notify(opts.subject, opts.body, TAUTULLI_API_KEY, TAUTULLI_NOTIFIER_ID, TAUTULLI_URL)
+    notify(opts.subject, opts.body, TAUTULLI_API_KEY, TAUTULLI_NOTIFIER_ID, TAUTULLI_URL)
